549dba9487a7a36a4745575d569645f6f44b7daf
荘司 里樹 <k00037@BZ4N-136.local>
2025-12-07 23:19:01 +0900

    update content

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// ▼ 1. この行を追加してください
+import { Analytics } from "@vercel/analytics/react"
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* ▼ 2. ここに Analytics コンポーネントを追加します */}
+    <Analytics />
   </StrictMode>,
 )
